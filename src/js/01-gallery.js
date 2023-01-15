aea4507e26e48090488761f9f62777a72645b74a
unknown <Taras>
2023-01-15 01:20:01 +0100

    implement simplelightbox task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemEls = galleryItems.map(item => buildGalleryItem(item));
+galleryEl.append(...galleryItemEls);
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+function buildGalleryItem({ preview, original, description }) {
+  const galleryItemEl = document.createElement('a');
+
+  galleryItemEl.classList.add('gallery__item');
+  galleryItemEl.href = original;
+  galleryItemEl.innerHTML = `
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+  `;
+
+  return galleryItemEl;
+}
